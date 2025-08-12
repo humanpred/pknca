2049f13eae719f28cd2a0560d9db00d69f93ede2
Gero1999 <gerardojoro@gmail.com>
2025-08-12 09:50:31 +0200

    add volpk parameter

--- R/pk.calc.urine.R
+++ R/pk.calc.urine.R
@@ -1,3 +1,24 @@
+#' Calculate the total urine volume
+#'
+#' @param volume The volume (or mass) of the sample
+#' @return The sum of urine volumes for the interval
+#' @export
+pk.calc.volpk <- function(volume) {
+  sum(volume, na.rm = TRUE)
+}
+add.interval.col("volpk",
+                 FUN="pk.calc.volpk",
+                 values=c(FALSE, TRUE),
+                 unit_type="volume",
+                 pretty_name="Total Urine Volume",
+                 desc="The sum of urine volumes for the interval")
+PKNCA.set.summary(
+  name="volpk",
+  description="geometric mean and geometric coefficient of variation",
+  point=business.geomean,
+  spread=business.geocv
+)
+
 #' Calculate amount excreted (typically in urine or feces)
 #'
 #' @details ae is `sum(conc*volume)`.
