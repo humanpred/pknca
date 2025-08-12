26a72b716461241d0648e7dfd6733ae4cc31c997
Gero1999 <gerardojoro@gmail.com>
2025-08-12 09:50:50 +0200

    add test for volpk parameter

--- tests/testthat/test-pk.calc.urine.R
+++ tests/testthat/test-pk.calc.urine.R
@@ -1,3 +1,10 @@
+test_that("pk.calc.volpk", {
+  expect_equal(pk.calc.volpk(c(1, 2, 3)), 6)
+  expect_equal(pk.calc.volpk(c(1, NA, 3)), 4)
+  expect_equal(pk.calc.volpk(NA), 0)
+  expect_equal(pk.calc.volpk(numeric()), 0)
+})
+
 test_that("pk.calc.ae", {
   expect_equal(
     pk.calc.ae(conc=1:5, volume=1:5),
