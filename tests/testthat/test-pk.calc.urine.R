d8690fb0ddf0766d70e131957f0d073111efe4e9
Gero1999 <gerardojoro@gmail.com>
2025-10-27 07:02:27 +0100

    test: add tests for ermax, ertmax, ertlst

--- tests/testthat/test-pk.calc.urine.R
+++ tests/testthat/test-pk.calc.urine.R
@@ -53,3 +53,49 @@ test_that("pk.calc.fe", {
                0.3,
                info="fe is calculated correctly with both vector/scalar")
 })
+
+test_that("pk.calc.ertlst", {
+  # All NA
+  expect_true(is.na(pk.calc.ertlst(conc = c(NA, NA), volume = c(1, 1), time = c(0, 1), duration.conc = c(1, 1))))
+  # All 0 or NA
+  expect_equal(pk.calc.ertlst(conc = c(0, NA), volume = c(1, 1), time = c(0, 1), duration.conc = c(1, 1)), 0)
+  # Normal case
+  expect_equal(
+    pk.calc.ertlst(conc = c(1, 2, 0), volume = c(1, 1, 1), time = c(0, 1, 2), duration.conc = c(1, 1, 1)),
+    max(c(0, 1) + 1/2)
+  )
+})
+
+test_that("pk.calc.ermax", {
+  # All NA
+  expect_true(is.na(pk.calc.ermax(conc = c(NA, NA), volume = c(1, 1), time = c(0, 1), duration.conc = c(1, 1))))
+  # Normal case
+  expect_equal(
+    pk.calc.ermax(conc = c(1, 2, 3), volume = c(2, 2, 2), time = c(0, 1, 2), duration.conc = c(2, 2, 2)),
+    max(c(1, 2, 3) * 2 / 2)
+  )
+})
+
+test_that("pk.calc.ertmax", {
+  # All NA or 0
+  expect_true(is.na(pk.calc.ertmax(conc = c(NA, 0), volume = c(1, 1), time = c(0, 1), duration.conc = c(1, 1))))
+  # Normal case, last tmax
+  expect_equal(
+    pk.calc.ertmax(conc = c(1, 3, 2), volume = c(2, 2, 2), time = c(0, 1, 2), duration.conc = c(2, 2, 2), first.tmax = FALSE),
+    (1 + 2/2)
+  )
+  # Normal case, first tmax
+  expect_equal(
+    pk.calc.ertmax(conc = c(1, 3, 2), volume = c(2, 2, 2), time = c(0, 1, 2), duration.conc = c(2, 2, 2), first.tmax = TRUE),
+    (1 + 2/2)
+  )
+  # Multiple maxima
+  expect_equal(
+    pk.calc.ertmax(conc = c(1, 3, 3), volume = c(2, 2, 2), time = c(0, 1, 2), duration.conc = c(2, 2, 2), first.tmax = TRUE),
+    (1 + 2/2)
+  )
+  expect_equal(
+    pk.calc.ertmax(conc = c(1, 3, 3), volume = c(2, 2, 2), time = c(0, 1, 2), duration.conc = c(2, 2, 2), first.tmax = FALSE),
+    (2 + 2/2)
+  )
+})
