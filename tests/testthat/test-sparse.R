1c45fe3aa4cf92ecedff9c0d1520c8efbfe924a0
Gero1999 <gerardojoro@gmail.com>
2025-11-24 18:32:51 +0100

    test: ignore_attr when relevant

--- tests/testthat/test-sparse.R
+++ tests/testthat/test-sparse.R
@@ -21,13 +21,13 @@ test_that("sparse_auc", {
     regexp="Cannot yet calculate sparse degrees of freedom for multiple samples per subject"
   )
   expect_equal(sparse_batch$sparse_auc, auclast, ignore_attr = TRUE)
-  expect_equal(sparse_batch$sparse_auc_se, auclast_se_batch)
-  expect_equal(sparse_batch$sparse_auc_df, NA_real_)
+  expect_equal(sparse_batch$sparse_auc_se, auclast_se_batch, ignore_attr = TRUE)
+  expect_equal(sparse_batch$sparse_auc_df, NA_real_, ignore_attr = TRUE)
 
   sparse_serial <- pk.calc.sparse_auc(conc=d_sparse$conc, time=d_sparse$time, subject=seq_len(nrow(d_sparse)))
   expect_equal(sparse_serial$sparse_auc, auclast, ignore_attr = TRUE)
   expect_equal(as.numeric(sparse_serial$sparse_auc_se), auclast_se_serial)
-  expect_equal(sparse_serial$sparse_auc_df, auclast_df_serial)
+  expect_equal(sparse_serial$sparse_auc_df, auclast_df_serial, ignore_attr = TRUE)
 })
 
 test_that("sparse_auclast expected errors", {
